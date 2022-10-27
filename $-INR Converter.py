7cb9b33c4ab30b97525163eac105ad7f50b250aa
MoonLightProf <116825577+MoonLightProf@users.noreply.github.com>
2022-10-27 20:53:14 +0530

    Create $-INR Converter.py

---
+++ $-INR Converter.py
@@ -0,0 +1,29 @@
+question_1 = input("Hey. To start the money conversion. Type 'money' and then follow the instructions given by the program. ")
+while question_1 == "money":
+    if question_1.lower() == 'money':
+        print("sure")
+    currency_1 = input('$ or INR? ')
+    currency_2 = float(input("Enter the numerical value for example 2 or 3 or 5: "))
+    break
+while currency_1.lower() == "$":
+    if currency_1.lower() == "$":
+        print(currency_2 * 82.35)
+        print('The value which had been given by you is now been converted to $.')
+    break
+while currency_1.lower() == "inr":
+    if currency_1.lower() == 'inr':
+        print(currency_2 / 82.35)
+        break
+
+
+#**THIS IS THE END OF THE CODE** 
+#**HE/SHE CAN'T MAKE CHANGES/EDIT THIS CODE WITHOUT THE AUTHOR'S PERMISSION
+#**YOU MAY SEEK FOR PERMISSION ON MY MAIL ID or DISCORD
+# spongesouth348@gmail.com <-- GMAIL ID
+# Yoriichi🔥#0709 <-- DISCORD ID.
+
+
+
+        
+
+
